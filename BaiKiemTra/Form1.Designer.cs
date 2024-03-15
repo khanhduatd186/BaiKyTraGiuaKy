cbba220336e3f2ab735957b020f073e58a69c703
hokhanhdu <khanhduatd1862002@gmail.com>
2024-03-15 14:31:33 +0700

    REVERT

--- BaiKiemTra/Form1.Designer.cs
+++ BaiKiemTra/Form1.Designer.cs
@@ -28,79 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtUserName = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.txtpassword = new System.Windows.Forms.TextBox();
-            this.btnlogin = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(109, 52);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(92, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "tên đăng nhập";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // txtUserName
-            // 
-            this.txtUserName.Location = new System.Drawing.Point(220, 52);
-            this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(312, 22);
-            this.txtUserName.TabIndex = 1;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(109, 112);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 16);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "mật khẩu";
-            // 
-            // txtpassword
-            // 
-            this.txtpassword.Location = new System.Drawing.Point(220, 112);
-            this.txtpassword.Name = "txtpassword";
-            this.txtpassword.Size = new System.Drawing.Size(312, 22);
-            this.txtpassword.TabIndex = 3;
-            // 
-            // btnlogin
-            // 
-            this.btnlogin.Location = new System.Drawing.Point(220, 194);
-            this.btnlogin.Name = "btnlogin";
-            this.btnlogin.Size = new System.Drawing.Size(75, 23);
-            this.btnlogin.TabIndex = 4;
-            this.btnlogin.Text = "button1";
-            this.btnlogin.UseVisualStyleBackColor = true;
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnlogin);
-            this.Controls.Add(this.txtpassword);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtUserName);
-            this.Controls.Add(this.label1);
-            this.Name = "Form1";
             this.Text = "Form1";
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtUserName;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox txtpassword;
-        private System.Windows.Forms.Button btnlogin;
     }
 }
 
